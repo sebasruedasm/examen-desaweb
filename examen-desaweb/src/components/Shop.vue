d076cb2117760d52cf8a2970b932873511fb51dc
sebasruedasm <sruedasm@ufpso.edu.co>
2022-12-20 23:32:54 -0500

    Todo funciona

--- examen-desaweb/src/components/Shop.vue
+++ examen-desaweb/src/components/Shop.vue
@@ -13,9 +13,9 @@
         <tr v-for="(producto, index) in productos" :key="index">
           <td><img :src="producto.link" /></td>
           <td>{{ producto.nombre }}</td>
-          <td>{{ producto.precio }}</td>
+          <td>$ {{ producto.precio }}</td>
           <td><input type="text" name="" id="" v-model="cantidad[index]" /></td>
-          <td>{{ subPrecio(index) }}{{ subProducto[index] }}</td>
+          <td>$ {{ calcularSubTotal(index) }}{{ subProducto[index] }}</td>
           <td><button @click="eliminarProducto(index)">X</button></td>
         </tr>
         <tr>
@@ -24,7 +24,7 @@
           <td></td>
           <td></td>
           <td>SUB TOTAL:</td>
-          <td>{{ calcularSubTotal() }}{{ subtotal }}</td>
+          <td>$ {{ calcularSubTotal1() }}{{ subTotal }}</td>
         </tr>
 
         <tr>
@@ -33,7 +33,7 @@
           <td></td>
           <td></td>
           <td>IVA:</td>
-          <td>{{ calcularIva() }}{{ iva }}</td>
+          <td>$ {{ calcularIva() }}{{ iva }}</td>
         </tr>
         <tr>
           <td></td>
@@ -41,7 +41,7 @@
           <td></td>
           <td></td>
           <td>TOTAL:</td>
-          <td>{{ calcularTotal() }}{{ total }}</td>
+          <td>$ {{ calcularTotal() }}{{ total }}</td>
         </tr>
         <tr>
           <td></td>
@@ -73,35 +73,39 @@ export default {
           precio: 4000,
         },
         {
-          link: "https://cdn.shopify.com/s/files/1/0604/8373/1606/products/IMG-104966_1445x.jpg?v=1658325800",
-          nombre: "Apple",
+          link: "https://acortar.link/fLZbUf",
+          nombre: "Lenovo",
+          precio: 4000,
+        },
+        {
+          link: "https://www.hp.com/gb-en/shop/Html/Merch/Images/c07964576_1750x1285.jpg",
+          nombre: "HP",
           precio: 4000,
         },
       ],
     };
   },
   methods: {
-    subtotal(index) {
+    calcularSubTotal(index) {
       if (this.cantidad[index] != "") {
         this.subProducto[index] =
-          parseInt(this.cantidad[index]) *
-          parseInt(this.productos[index].precio);
+          parseInt(this.cantidad[index]) * this.productos[index].precio;
       } else {
         this.subProducto[index] = 0;
       }
     },
     calcularIva() {
-      this.iva = this.subtot * 0.19;
+      this.iva = this.subTotal * 0.19;
     },
-    calcularSubTotal() {
+    calcularSubTotal1() {
       let sumar = 0;
       for (var i = 0; i < this.subProducto.length; i++) {
         sumar = sumar + this.subProducto[i];
       }
-      this.subtot = sumar;
+      this.subTotal = sumar;
     },
-    calculoTotal() {
-      this.total = this.subtot + this.iva;
+    calcularTotal() {
+      this.total = this.subTotal + this.iva;
     },
     eliminarProducto(index) {
       return this.productos.splice(index, 1);
